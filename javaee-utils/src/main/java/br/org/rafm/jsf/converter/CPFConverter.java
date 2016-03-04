52bc7039a32514afd7fa798d804a9cb1863d8e27
Rafael Augusto <rafael.augusto.fm@outlook.com>
2016-03-04 16:26:52 +0000

    Criando JSF "CPFConverter"

---
+++ javaee-utils/src/main/java/br/org/rafm/jsf/converter/CPFConverter.java
@@ -0,0 +1,28 @@
+package br.org.rafm.jsf.converter;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+@FacesConverter("rafm.CPFConverter")
+public class CPFConverter implements Converter {
+
+	@Override
+	public Object getAsObject(FacesContext context, UIComponent component, String value) {
+		return value != null ? value.replaceAll("\\D", "") : null;
+	}
+
+	@Override
+	public String getAsString(FacesContext context, UIComponent component, Object value) {
+		if (value == null) {
+			return "";
+		}
+		
+		if (value instanceof String && ((String) value).length() == 11) {
+			value = ((String) value).substring(0, 3)+"."+((String) value).substring(3, 6)+"."+((String) value).substring(6, 9)+"-"+((String) value).substring(9, 11);
+		}
+		
+		return value.toString();
+	}
+}
